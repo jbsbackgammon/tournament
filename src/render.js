f1a5e27ca9c06a446315a2c9e4603d63a5cbc1c8
jbsbackgammon <jbsbackgammon@users.noreply.github.com>
2026-09-24 15:17:25 +0900

    Match right supplement gap to left block

--- src/render.js
+++ src/render.js
@@ -82,7 +82,7 @@ export function renderBracket(state, options = {}) {
       if (state.showMatchNotes) supplementParts.push(supplementText(note, midX, p.y + font * .2, Math.abs(dest.x - p.x) - 20, p.side ? 'start' : 'end'));
       if (state.showResultNotes) {
         const align = p.side ? 'end' : 'start';
-        const x = p.side ? p.x - 2 : p.x + 10;
+        const x = p.side ? p.x - 10 : p.x + 10;
         supplementParts.push(supplementText(upperResult, x, nodes[r][i].y - line * 1.2, Math.abs(dest.x - p.x) - 20, align));
         supplementParts.push(supplementText(lowerResult, x, nodes[r][i + 1].y + line * 2.2, Math.abs(dest.x - p.x) - 20, align));
       }
