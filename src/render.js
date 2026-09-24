98ac36bde6564e5737afd67535e68bae077db545
jbsbackgammon <jbsbackgammon@users.noreply.github.com>
2026-09-24 15:26:35 +0900

    Place final result supplements above final bars

--- src/render.js
+++ src/render.js
@@ -90,8 +90,8 @@ export function renderBracket(state, options = {}) {
     if (state.showResultNotes) {
       const finalNotes = state.resultNotes?.[state.rounds.length - 1] || [];
       const finalY = nodes.at(-1)[0].y;
-      supplementParts.push(supplementText(finalNotes[0], W / 2 - 12, finalY + font * .2, W / 2 - 40, 'end'));
-      supplementParts.push(supplementText(finalNotes[1], W / 2 + 12, finalY + font * .2, W / 2 - 40, 'start'));
+      supplementParts.push(supplementText(finalNotes[0], nodes.at(-1)[0].x + 10, finalY - line * 1.2, W / 2 - 40, 'start'));
+      supplementParts.push(supplementText(finalNotes[1], nodes.at(-1)[1].x - 10, finalY - line * 1.2, W / 2 - 40, 'end'));
     }
   }
   const finalY = nodes.at(-1)[0].y;
